b1dea97f8eb8a09dfa6408a6d7e3cb13a6d37705
Yunusemreyilandil <yunusemreyilandil61@hotmail.com>
2024-10-30 14:13:34 +0300

    Update

--- asragame/Assets/Scripts/PlayerController.cs
+++ asragame/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10f;
     private Rigidbody rb;
     private bool isGrounded;
+    private bool canStopTime = false;
+    private bool timeStopped = false;
 
     void Start()
     {
@@ -15,15 +17,54 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        HandleMovement();
+        HandleJump();
+        HandleTimeStop();
+    }
+
+    private void HandleMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
+
+        // Zaman durduðunda unscaledDeltaTime kullanarak oyuncunun hareket etmesini saðlýyoruz
         Vector3 move = new Vector3(horizontalInput * moveSpeed, rb.velocity.y, 0);
         rb.velocity = move;
+    }
 
+    private void HandleJump()
+    {
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGrounded = false;
         }
+    }
+
+    private void HandleTimeStop()
+    {
+        if (canStopTime && Input.GetKeyDown(KeyCode.T)) // 'T' tuþu ile zamaný durdur
+        {
+            ToggleTimeStop();
+        }
+    }
+
+    private void ToggleTimeStop()
+    {
+        if (timeStopped)
+        {
+            Time.timeScale = 1; // Zamaný devam ettir
+            timeStopped = false;
+        }
+        else
+        {
+            Time.timeScale = 0; // Zamaný durdur
+            timeStopped = true;
+        }
+    }
+
+    public void EnableTimeStop()
+    {
+        canStopTime = true; // Zaman durdurma yeteneðini aktif eder
     }
 
     private void OnCollisionEnter(Collision collision)
