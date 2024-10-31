ac9e29d78f94f77325d62ff94e6f2b703544066b
Yunusemreyilandil <yunusemreyilandil61@hotmail.com>
2024-10-31 15:21:38 +0300

    Update

--- asragame/Assets/Scripts/PlayerController.cs
+++ asragame/Assets/Scripts/PlayerController.cs
@@ -4,26 +4,46 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float jumpForce = 10f;
+    public float crouchSpeed = 2.5f;  
     private Rigidbody rb;
     private bool isGrounded;
+    private bool isCrouching;
+
+    private Vector3 originalScale;  
+    private float crouchScaleY = 0.5f;  
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+        originalScale = transform.localScale;  
     }
 
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
 
-        // Zamandan baðýmsýz hareket
-        Vector3 move = new Vector3(horizontalInput * moveSpeed, rb.velocity.y, 0);
+        
+        if (Input.GetKeyDown(KeyCode.C) && isGrounded)
+        {
+            isCrouching = true;
+            transform.localScale = new Vector3(originalScale.x, crouchScaleY, originalScale.z);  
+        }
+        else if (Input.GetKeyUp(KeyCode.C))
+        {
+            isCrouching = false;
+            transform.localScale = originalScale;  
+        }
 
-        // unscaledTime ile X eksenindeki hýz, zaman yavaþlatmadan etkilenmeyecek
+        
+        float currentSpeed = isCrouching ? crouchSpeed : moveSpeed;
+        Vector3 move = new Vector3(horizontalInput * currentSpeed, rb.velocity.y, 0);
+
+        
         rb.velocity = new Vector3(move.x * Time.timeScale, rb.velocity.y, rb.velocity.z);
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded && !isCrouching)
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGrounded = false;
