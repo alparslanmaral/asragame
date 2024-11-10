3660849210b29c95d62cb9c8cd5cbef36545eb4b
Ahmet Alparslan MARAL <alpik36@gmail.com>
2024-11-10 04:11:03 +0300

    comic book assetleri, main menu, ui ve player fix

---
+++ asragame/Assets/Asra/Chat/ProximityActivator.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class ProximityActivator : MonoBehaviour
+{
+    public GameObject targetCanvas; // Aktif/pasif yapacaðýmýz Canvas
+
+    private void Start()
+    {
+        // Baþlangýçta Canvas'ý devre dýþý yap
+        if (targetCanvas != null)
+        {
+            targetCanvas.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Player tag'li obje Trigger alanýna girerse Canvas'ý etkinleþtir
+        if (other.CompareTag("Player") && targetCanvas != null)
+        {
+            targetCanvas.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Player tag'li obje Trigger alanýndan çýkarsa Canvas'ý devre dýþý býrak
+        if (other.CompareTag("Player") && targetCanvas != null)
+        {
+            targetCanvas.SetActive(false);
+        }
+    }
+}
---
+++ asragame/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public string YuklenecekSahne = "SahneAdiYaz"; // AnaMap sahnesinin adý
+
+    public void ReturnToMainMap()
+    {
+        SceneManager.LoadScene(YuklenecekSahne); // AnaMap sahnesine geçiþ yap
+    }
+}
